6a60d4f69a5fabd4e254fd84ca57e8187bc9a7b1
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-10-30 13:27:09 +0600

    fix: updated ui in mobile sidebar

--- src/app/chat/components/MobileSidebar.tsx
+++ src/app/chat/components/MobileSidebar.tsx
@@ -3,16 +3,17 @@ import { Label } from "@/components/ui/label";
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
-  SheetTitle,
-  SheetTrigger,
 } from "@/components/ui/sheet";
-import SearchSelect from "./SelectUser";
+import SelectUser from "./SelectUser";
 import { Switch } from "@/components/ui/switch";
 import React from "react";
 import { Input } from "@/components/ui/input";
 import { useChatStore } from "@/lib/stores/chatStores";
 import SelectItem from "./SelectItem";
+import { XMarkIcon } from "@heroicons/react/20/solid";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const MobileSidebar = () => {
   const {
@@ -20,68 +21,80 @@ const MobileSidebar = () => {
     isGroupChat,
     groupParticipants,
     selectedUser,
+    isMobileSidebarOpen,
     updateGroupName,
     updateIsGroupChat,
+    toggleIsMobileSidebarOpen,
   } = useChatStore((state) => state);
 
   return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button variant="outline">Open</Button>
-      </SheetTrigger>
-      <SheetContent side="left">
-        <SheetHeader>
-          <SheetTitle>Talk Tide</SheetTitle>
-        </SheetHeader>
+    <Sheet open={isMobileSidebarOpen}>
+      <SheetContent className="flex flex-col justify-between" side="right">
+        <div>
+          <SheetHeader className="mb-4">
+            <div className="flex justify-between items-center">
+              <Avatar className="h-10 w-10">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>M</AvatarFallback>
+              </Avatar>
 
-        {/* toggle if the chat is group chat or one to one chat*/}
-        <div className="flex items-center space-x-2 mb-2">
-          <Switch
-            checked={isGroupChat}
-            onCheckedChange={updateIsGroupChat}
-            id="group-chat"
-          />
-          <Label htmlFor="group-chat">Is it a group chat?</Label>
-        </div>
-        {/* ----------------------------------------------------------*/}
+              <Button
+                onClick={toggleIsMobileSidebarOpen}
+                className="w-9 h-9 p-1 rounded-full"
+                variant="outline"
+              >
+                <XMarkIcon className="w-full h-full" />
+              </Button>
+            </div>
+          </SheetHeader>
 
-        {/* 
+          {/* toggle if the chat is group chat or one to one chat*/}
+          <div className="flex items-center space-x-2 mb-2">
+            <Switch
+              checked={isGroupChat}
+              onCheckedChange={updateIsGroupChat}
+              id="group-chat"
+            />
+            <Label htmlFor="group-chat">Is it a group chat?</Label>
+          </div>
+          {/* ----------------------------------------------------------*/}
+
+          {/* 
         map the selected group participants avatar or
         show a single user avatar 
         */}
-        <div className="flex flex-wrap gap-1  mb-2">
-          {isGroupChat
-            ? groupParticipants.map((user, index) => (
-                <SelectItem key={index} user={user} />
-              ))
-            : selectedUser && <SelectItem user={selectedUser} />}
-        </div>
-        {/* --------------------------------------------------- */}
+          <div className="flex flex-wrap gap-1  mb-2">
+            {isGroupChat
+              ? groupParticipants.map((user, index) => (
+                  <SelectItem key={index} user={user} />
+                ))
+              : selectedUser && <SelectItem user={selectedUser} />}
+          </div>
+          {/* --------------------------------------------------- */}
 
-        {/* 
+          {/* 
             if it is group chat then toggle the input field for
             group name
         */}
-        {isGroupChat && (
-          <Input
-            value={groupName}
-            onChange={(e) => updateGroupName(e.target.value)}
-            type="text"
-            placeholder="Group name..."
-            className="mb-2 border-gray-300"
-          />
-        )}
-        {/* -------------------------------------------------------- */}
+          {isGroupChat && (
+            <Input
+              value={groupName}
+              onChange={(e) => updateGroupName(e.target.value)}
+              type="text"
+              placeholder="Group name..."
+              className="mb-2 border-gray-300"
+            />
+          )}
+          {/* -------------------------------------------------------- */}
 
-        {/* select user for chat */}
-        <SearchSelect />
-        {/* --------------------- */}
+          {/* select user for chat */}
+          <SelectUser />
+          {/* --------------------- */}
+        </div>
 
-        {/* <SheetFooter>
-          <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
-          </SheetClose>
-        </SheetFooter> */}
+        <SheetFooter>
+          <h2 className="text-center">TALK TIDE</h2>
+        </SheetFooter>
       </SheetContent>
     </Sheet>
   );
