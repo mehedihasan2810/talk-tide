e37a6f6360edc7212b8b283bc3440ab6bfa6528a
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-11-03 19:30:55 +0600

    feat: add Custom error class for handling error easily

---
+++ src/utils/error-helpers/ApiError.ts
@@ -0,0 +1,30 @@
+class ApiError extends Error {
+  statusCode: number;
+  message: string;
+  errors: string[];
+  success: boolean;
+  data: any | null;
+  stack?: string | undefined;
+
+  constructor(
+    statusCode: number,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
