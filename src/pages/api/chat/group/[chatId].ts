c8de948a32cf84e2e5e1656af23f684bde444b18
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-11-04 13:37:36 +0600

    feat: add route controller to rename or update group chat name

--- src/pages/api/chat/group/[chatId].ts
+++ src/pages/api/chat/group/[chatId].ts
@@ -1,4 +1,6 @@
 import { getGroupChatDetails } from "@/socket/controllers/chat-controllers/getGroupChatDetails";
+import renameGroupChat from "@/socket/controllers/chat-controllers/renameGroupChat";
+import { startSocketServer } from "@/socket/startSocketServer";
 import { NextApiResponseServerIO } from "@/types/types";
 import { errorResponse } from "@/utils/error-helpers/errorResponse";
 import { NextApiRequest } from "next";
@@ -9,11 +11,21 @@ export default async function handler(
 ) {
   try {
     // start socket server
-    // startSocketServer(req, res);
+    startSocketServer(req, res);
 
     // --------------------------------
-    await getGroupChatDetails(req, res);
+
+    if (req.method === "GET") {
+      await getGroupChatDetails(req, res);
+    }
+
     // ---------------------------------
+
+    if (req.method === "PATCH") {
+      await renameGroupChat(req, res);
+    }
+
+    // ----------------------------------
   } catch (error) {
     const errorRes = errorResponse(error);
     res.status(errorRes.statusCode).json(errorRes);
---
+++ src/socket/controllers/chat-controllers/renameGroupChat.ts
@@ -0,0 +1,119 @@
+import prisma from "@/lib/prisma";
+import { ChatEventEnum } from "@/socket/constants";
+import { emitSocketEvent } from "@/socket/socket-events/emitSocketEvent";
+import { NextApiResponseServerIO } from "@/types/types";
+import { ApiError } from "@/utils/error-helpers/ApiError";
+import { ApiResponse } from "@/utils/helpers/apiResponse";
+import { NextApiRequest } from "next";
+
+const renameGroupChat = async (
+  req: NextApiRequest,
+  res: NextApiResponseServerIO
+) => {
+  const { chatId } = req.query;
+  const { name } = req.body;
+
+  // throw error if theres no chat id ------------
+  if (!chatId || chatId === undefined) {
+    throw new ApiError(400, "chat id is missing!");
+  }
+  // ---------------------------------------------
+
+  // throw error if theres no name ------------
+  if (!name || name === undefined) {
+    throw new ApiError(400, "group name is missing is missing!");
+  }
+  // ---------------------------------------------
+
+  // check for chat existence
+  const groupChat = await prisma.chat.findUnique({
+    where: {
+      id: chatId as string,
+      isGroupChat: true,
+    },
+  });
+  //   --------------------------------------------
+
+  if (!groupChat) {
+    throw new ApiError(404, "Group chat does not exist");
+  }
+
+  // only admin can change the name
+  if (groupChat.adminId !== req.cookies.id) {
+    throw new ApiError(404, "You are not an admin");
+  }
+
+  //   update the group name and retrieve necessary data
+  const updateGroupChat = await prisma.chat.update({
+    where: {
+      id: chatId as string,
+    },
+    data: {
+      name,
+    },
+    include: {
+      // --------------
+      participants: {
+        select: {
+          id: true,
+          username: true,
+          email: true,
+          avatar: true,
+          role: true,
+          loginType: true,
+          isEmailVerified: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      },
+      // ---------------------
+      chatMessages: {
+        orderBy: {
+          createdAt: "desc",
+        },
+        take: 1,
+
+        include: {
+          sender: {
+            select: {
+              username: true,
+              avatar: true,
+              email: true,
+            },
+          },
+        },
+      },
+      // --------------------------
+    },
+  });
+  //   ----------------------------------------------------
+
+  if (!updateGroupChat) {
+    throw new ApiError(500, "Internal server error");
+  }
+
+  // logic to emit socket event about the updated chat name to the participants
+  updateGroupChat.participants.forEach((participant) => {
+    // emit event to all the participants with updated chat as a payload
+    emitSocketEvent(
+      res.socket.server.io,
+      participant.id,
+      ChatEventEnum.UPDATE_GROUP_NAME_EVENT,
+      updateGroupChat
+    );
+  });
+  //   --------------------------------------------------------
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        updateGroupChat,
+        "Group chat name updated successfully"
+      )
+    );
+  // ---------------------------------------
+};
+
+export default renameGroupChat;
