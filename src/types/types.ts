4272b0751251a56d387129903282bce68c266418
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-11-07 23:28:17 +0600

    feat: types for session data

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,6 @@
 import { Server as NetServer, Socket } from "net";
 import { NextApiResponse } from "next";
+import { DefaultSession } from "next-auth";
 import { Server as SocketIOServer } from "socket.io";
 
 export type NextApiResponseServerIO = NextApiResponse & {
@@ -8,4 +9,15 @@ export type NextApiResponseServerIO = NextApiResponse & {
       io: SocketIOServer;
     };
   };
+};
+
+type DefaultSessionUser = NonNullable<DefaultSession["user"]>;
+
+type SessionUser = DefaultSessionUser & {
+  id: string;
+  role: string;
+};
+
+export type Session = DefaultSession & {
+  user?: SessionUser;
 };
