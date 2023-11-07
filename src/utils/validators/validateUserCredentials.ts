c899c17a779564d9b79f521dfd9be4f7f43a43ac
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-11-07 23:03:59 +0600

    feat: add functionality for validating register and login credentials

---
+++ src/utils/validators/validateUserCredentials.ts
@@ -0,0 +1,29 @@
+import { LoginSchema } from "../zod-schema/loginSchema";
+import { RegisterSchema } from "../zod-schema/registerSchema";
+
+interface Params {
+  authType: string;
+  username: string;
+  email?: string;
+  password: string;
+}
+
+export const validateUserCredentials = (data: Params) => {
+  const result = (
+    data.authType === "register" ? RegisterSchema : LoginSchema
+  ).safeParse(data);
+
+  if (!result.success) {
+    const extractedErrors = result.error.issues.map(
+      (err: (typeof result.error.issues)[0]) => {
+        return err.message;
+      }
+    );
+    console.log(extractedErrors);
+
+    throw new Error(extractedErrors.join(","));
+    // ------------------------------------------
+  } else {
+    console.log(result.data);
+  }
+};
