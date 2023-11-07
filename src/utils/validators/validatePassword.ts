7599533403a91395af35050082ca8386a59eac1d
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-11-07 23:20:49 +0600

    feat: add functionality to hash and validate password

---
+++ src/utils/validators/validatePassword.ts
@@ -0,0 +1,23 @@
+import "server-only";
+import bcrypt from "bcrypt";
+
+export const hashPassword = async (password: string): Promise<string> => {
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return hashedPassword;
+  } catch (error) {
+    throw new Error("Invalid password");
+  }
+};
+
+export const verifyPassword = async (
+  password: string,
+  hashedPassword: string
+): Promise<boolean> => {
+  try {
+    const isValid = await bcrypt.compare(password, hashedPassword);
+    return isValid;
+  } catch (error) {
+    throw new Error("Invalid Password");
+  }
+};
