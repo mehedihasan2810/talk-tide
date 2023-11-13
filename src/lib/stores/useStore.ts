d5b57aaed9eb86fee1c6bdaf0dabe3ffce729453
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-11-13 22:59:08 +0600

    feat: add socket store slice into main store

--- src/lib/stores/useStore.ts
+++ src/lib/stores/useStore.ts
@@ -3,9 +3,16 @@ import {
   CreateAsyncSlice,
   createAsyncSlice,
 } from "./storeSlices/createAsyncSlice";
+import {
+  createSocketSlice,
+  type CreateSocketSlice,
+} from "./storeSlices/createSocketSlice";
 import { create } from "zustand";
 
-export const useStore = create<CreatChatSlice & CreateAsyncSlice>()((...a) => ({
+export const useStore = create<
+  CreatChatSlice & CreateAsyncSlice & CreateSocketSlice
+>()((...a) => ({
   ...createChatSlice(...a),
   ...createAsyncSlice(...a),
+  ...createSocketSlice(...a),
 }));
