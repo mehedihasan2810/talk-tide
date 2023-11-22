d9922ceb26f11f34058476f6ce22eb3cf3441fae
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-11-22 23:32:08 +0600

    feat: add functionality of removing a participant from a group chat

---
+++ src/app/chat/hooks/mutations/useRemoveParticipant.ts
@@ -0,0 +1,21 @@
+import { apiClient } from "@/lib/api";
+import { ChatInterface } from "@/types/chat";
+import { requestHandler } from "@/utils/requestHandler";
+import { useMutation } from "@tanstack/react-query";
+
+export const useRemoveParticipant = () => {
+  return useMutation({
+    mutationFn: ({
+      chatId,
+      participantToBeRemoved,
+    }: {
+      chatId: string;
+      participantToBeRemoved: string;
+    }) =>
+      requestHandler<ChatInterface>(() =>
+        apiClient.delete(
+          `/chat/group/addParticipant/${chatId}/${participantToBeRemoved}`,
+        ),
+      )(),
+  });
+};
