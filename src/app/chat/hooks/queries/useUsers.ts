87e52adee8cd4c6b9fd22997e9534af2a40e4256
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-11-22 23:27:16 +0600

    feat: fetch all the users

---
+++ src/app/chat/hooks/queries/useUsers.ts
@@ -0,0 +1,13 @@
+import { apiClient } from "@/lib/api";
+import { UserInterface } from "@/types/user";
+import { requestHandler } from "@/utils/requestHandler";
+import { useQuery } from "@tanstack/react-query";
+
+export const useUsers = () => {
+  return useQuery({
+    queryKey: ["users"],
+    queryFn: requestHandler<UserInterface[]>(() =>
+      apiClient.get("/chat/users"),
+    ),
+  });
+};
