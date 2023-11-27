c7e53832342e7b241d03199766fda22dfe9baaf6
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-11-27 23:41:40 +0600

    fix:  improve security on initializing socket

--- src/socket/initializeSocketIO.ts
+++ src/socket/initializeSocketIO.ts
@@ -1,46 +1,66 @@
 import { ChatEventEnum } from "./constants";
-// import cookie from "cookie";
 import type { Server, Socket } from "socket.io";
 import { mountJoinChatEvent } from "./socket-events/mountJoinChatEvent";
 import { mountParticipantTypingEvent } from "./socket-events/mountParticipantTypingEvent";
 import { mountParticipantStoppedTypingEvent } from "./socket-events/mountParticipantStoppedTypingEvent";
 import { NextApiRequest } from "next";
 import { NextApiResponseServerIO } from "@/types/types";
+import { ApiError } from "@/utils/error-helpers/ApiError";
+import prisma from "@/lib/prisma";
 
-// todo
 type NextApiRequestWithPayload = NextApiRequest & { user?: { id: string } };
 
+type SocketType = Socket & {
+  user?: {
+    id: string;
+    password: string;
+    refreshToken: string | null;
+    emailVerificationToken: string | null;
+    emailVerificationExpiry: Date | null;
+  };
+};
+
 const initializeSocketIO = (
-  req: NextApiRequestWithPayload,
-  res: NextApiResponseServerIO
+  _req: NextApiRequestWithPayload,
+  res: NextApiResponseServerIO,
 ) => {
   const io: Server = res.socket.server.io;
-  return io.on(ChatEventEnum.CONNECTION_EVENT, async (socket: Socket) => {
+  return io.on(ChatEventEnum.CONNECTION_EVENT, async (socket: SocketType) => {
     try {
-      //   // parse the cookies from the handshake headers (This is only possible if client has `withCredentials: true`)
-      //   const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
-      //   //   ------------------------------------------------------------------
+      const credentials = socket.handshake.auth.user;
 
-      //   //get the access token
-      //   let token = cookies?.accessToken;
-      //   // -----------------------------
+      if (!credentials) {
+        // credentials are required for the socket to work
+        throw new ApiError(401, "Un-authorized handshake. Invalid credentials");
+      }
 
-      //   // ------------------------------------------
-      //   if (!token) {
-      //     // if there is no access token in cookies. Check inside the handshake auth
-      //     token = socket.handshake.auth?.token;
-      //   }
-      //   //   ---------------------------------------------
+      // retrieve the user
+      const user = await prisma.user.findUnique({
+        where: {
+          id: credentials.id,
+        },
+        select: {
+          id: true,
+          password: true,
+          refreshToken: true,
+          emailVerificationToken: true,
+          emailVerificationExpiry: true,
+        },
+      });
 
-      // if (!token) {
-      //   // Token is required for the socket to work
-      //   throw new ApiError(401, "Un-authorized handshake. Token is missing");
-      // }
+      if (!user) {
+        throw new ApiError(401, "Un-authorized handshake. Invalid Credentials");
+      }
 
-      // ------------------------------------------
+      socket.user = user; // mount te user object to the socket
+
+      // We are creating a room with user id so that if user is joined but does not have any active chat going on.
+      // still we want to emit some socket events to the user.
+      // so that the client can catch the event and show the notifications.
+      socket.join(user.id);
+
       socket.emit(ChatEventEnum.CONNECTED_EVENT); // emit the connected event so that client is aware
-      console.log("User connected 🗼");
-      // ------------------------------------------
+      console.log("User connected 🗼. userId: ", user.id);
 
       // Common events that needs to be mounted on the initialization
       mountJoinChatEvent(socket);
@@ -51,19 +71,17 @@ const initializeSocketIO = (
       // DISCONNECTED EVENT -----------------------------
       socket.on(ChatEventEnum.DISCONNECT_EVENT, () => {
         console.log("user has disconnected 🚫. userId: ");
-        // if (socket.user?._id) {
-        //   socket.leave(socket.user._id);
-        // }
+        if (socket.user?.id) {
+          socket.leave(socket.user.id);
+        }
       });
       // -------------------------------------------------
-    } catch (error: unknown) {
-      // SOCKET ERROR EVENT ---------------------
+    } catch (error) {
       socket.emit(
         ChatEventEnum.SOCKET_ERROR_EVENT,
         (error as Error).message ||
-          "Something went wrong while connecting to the socket."
+          "Something went wrong while connecting to the socket.",
       );
-      // ----------------------------------------
     }
   });
 };
