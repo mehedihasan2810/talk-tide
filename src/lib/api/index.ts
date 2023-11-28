e661f811f2dbd70cdbebff0091ee8631184b6adb
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-11-28 19:18:17 +0600

    fix: removed localhost url as nextjs auto detects the url

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance for API requests
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URI,
+  baseURL: "/api",
   withCredentials: true,
   timeout: 120000,
 });
