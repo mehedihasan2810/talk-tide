be70a7ddddfff8e4645ebc6853c1a377563d73fa
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-12-03 00:09:05 +0600

    fix: fix eslint error

--- src/types/session.ts
+++ src/types/session.ts
@@ -1,15 +1,4 @@
-import { Server as NetServer, Socket } from "net";
-import { NextApiResponse } from "next";
 import { DefaultSession } from "next-auth";
-import { Server as SocketIOServer } from "socket.io";
-
-export type NextApiResponseServerIO = NextApiResponse & {
-  socket: Socket & {
-    server: NetServer & {
-      io: SocketIOServer;
-    };
-  };
-};
 
 type DefaultSessionUser = NonNullable<DefaultSession["user"]>;
 
